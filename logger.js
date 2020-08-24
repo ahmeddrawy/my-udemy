4014a62a8001fde049e2ba4088eec61bd1bd2062
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2020-08-24 20:42:01 +0200

    remove logger dummy middleware

--- logger.js
+++
@@ -1,7 +0,0 @@
-/// logger middleware for the express server
-function log(req , res , next) {
-    console.log('logging');
-    next(); /// next to pass control to the next middleware function in the pipeline
-            /// if we don't use this our request will end up hanging  
-}
-module.exports = log;
