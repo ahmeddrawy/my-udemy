556d8242902c5f4671642b35c5a229532231828a
Hanafy-ubuntu <ahmedhanfy1111@gmail.com>
2020-09-12 15:34:41 +0200

    post req to handle registeration

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,21 @@ const express = require('express');
 const router = express.Router();
 const {User , validate} = require('../models/user');
 /// to reqister a user 
-router.post('/',(req,res)=>{
-    
+router.post('/',async (req,res)=>{
+    const {error } = validate(req.body);
+    if(error){
+        res.status(500).send(error);
+        return ;
+    }
+    let user = new User(req.body);
+    try {
+
+        user = await user.save();
+        res.send(user);
+    }
+    catch(err){
+        res.send(err.message);
+    }
+
 });
+module.exports = router ;
