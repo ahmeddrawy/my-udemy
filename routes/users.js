0133c77b60629bc60eced837805ee293a92cad49
Hanafy-ubuntu <ahmedhanfy1111@gmail.com>
2020-09-12 23:02:23 +0200

    add and use lodash package to filter objects

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const {User , validate} = require('../models/user');
+const _ = require('lodash');
 /// to reqister a user 
 router.post('/',async (req,res)=>{
     const {error } = validate(req.body);
     if(error){
-        res.status(500).send(error);
+        res.status(400).send(error.details[0].message);
         return ;
     }
-    let user = new User(req.body);
+    let user = new User(_.pick(req.body, ['name','email','password']));
     try {
 
         user = await user.save();
+        user=  _.pick(user,['name','email']);
         res.send(user);
     }
     catch(err){
