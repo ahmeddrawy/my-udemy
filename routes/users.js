1c001c3d50258e07bf73b51cca1b895f235422a8
Hanafy-ubuntu <ahmedhanfy1111@gmail.com>
2020-09-13 12:16:46 +0200

    use bcrypt to encrypt password stored in db

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {User , validate} = require('../models/user');
 const _ = require('lodash');
+const bcrypt = require('bcrypt');
 /// to reqister a user 
 router.post('/',async (req,res)=>{
     const {error } = validate(req.body);
@@ -11,7 +12,8 @@ router.post('/',async (req,res)=>{
     }
     let user = new User(_.pick(req.body, ['name','email','password']));
     try {
-
+        const salt =await bcrypt.genSalt();
+        user.password = await bcrypt.hash(user.password,salt);
         user = await user.save();
         user=  _.pick(user,['name','email']);
         res.send(user);
