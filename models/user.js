b02bef5c44bc171d37ef19c2f7b526bbd1154a68
Hanafy-ubuntu <ahmedhanfy1111@gmail.com>
2020-09-15 13:07:16 +0200

    hash passwords before saving to db using bcrypt

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
+const bcrypt = require('bcrypt');
 const emailRegex = /^[\w]+[\w\.]+@([\w-])+(\.)+[\w-]{2,4}$/;
 const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
 const schema= new mongoose.Schema({
@@ -11,7 +12,19 @@ const schema= new mongoose.Schema({
     },
     password : {
         type:String,
-        match:passwordRegex
+        match:passwordRegex,
+        set:async function(v){
+            const salt =await bcrypt.genSalt(10);
+            console.log(v);
+            try {
+                const hashed =  await bcrypt.hash(v,salt);
+                console.log(hashed);
+                return hashed;
+                
+            } catch (error) {
+                console.log('error in setting password',error.message);
+            }
+        }
     },
     email:{
         type:String,
@@ -21,7 +34,6 @@ const schema= new mongoose.Schema({
 })
 const User = mongoose.model('user' ,schema);
 function validateUser(user){
-    console.log('validating  in joi ', user);
     const schema = Joi.object({
         name:Joi.string().min(3).required(),
         password:Joi.string().min(8).regex(passwordRegex),
@@ -29,6 +41,9 @@ function validateUser(user){
     });
     return schema.validate(user);
 }
+schema.set('toObject', { getters: true });
+schema.set('toJSON', { getters: true });
+
 module.exports= {
     User :User ,
     validate : validateUser
