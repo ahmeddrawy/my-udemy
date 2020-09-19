94fe9bbf729f71395857bafabe09905e9e70bb94
Hanafy-ubuntu <ahmedhanfy1111@gmail.com>
2020-09-19 22:57:22 +0200

    use  error middleware and remove config

--- app.js
+++ app.js
@@ -3,21 +3,21 @@ const app = express();
 const Joi = require('joi'); //return class
 const mongoose = require('mongoose');
 
+/// routes
 const courses = require('./routes/courses');
 const genres = require('./routes/genres');
 const users = require('./routes/users');
 const logins = require('./routes/logins');
+
+///middlewares
+const err_middleware = require('./middleware/error');
 
 //======== db setup ================
 const URI = 'mongodb://localhost:27017/MyUdemy';
 mongoose.connect(URI)
 .then(()=>console.log('connected to mongodb'))
 .catch((err)=>console.log(`can't connect` , err.message));
-//====================
-// const config = require('config');
 
-/// ============= config ============
-// console.log(`app name ${config.get('DEBUG')}`);
 app.use(express.json());
 ///============= routes =============
 app.use('/api/courses',courses);
@@ -25,6 +25,8 @@ app.use('/api/genres',genres);
 app.use('/api/users',users);
 app.use('/api/logins',logins);
 
+// middlewares
+app.use(err_middleware);
 
 app.get('/',(req ,res)=>{
     res.send('hello world');
