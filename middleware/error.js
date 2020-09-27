9eec52e33aa5dfbc1f7f0c5e1bd6ec99757d60a8
Hanafy-ubuntu <ahmedhanfy1111@gmail.com>
2020-09-27 17:50:06 +0200

    use winston to log errors in middleware

--- middleware/error.js
+++ middleware/error.js
@@ -1,4 +1,6 @@
+const winston = require('winston');
 module.exports = function (err, req,res ,next){
     /// log the error 
+    winston.error(err.message,err)
     res.status(500).send('sorry we have an internal error')
 }
