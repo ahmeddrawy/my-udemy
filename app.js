ea66d0f8991be19d9b800709f12cf2e94659bb2a
Hanafy-ubuntu <ahmedhanfy1111@gmail.com>
2020-09-29 18:56:31 +0200

    log port info using winston

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const winston = require('winston');
 require('./startup/logging')(); /// logging is here to handle errors from routes and dbs
 require('./startup/routes')(app);
 require('./startup/db')();
@@ -7,5 +8,5 @@ require('./startup/config')();
 
 const port = process.env.PORT || 8080;
 app.listen(port,()=>{
-    console.log(`listening to port ${port}`);
+    winston.info(`listening to port ${port}`);
 });
