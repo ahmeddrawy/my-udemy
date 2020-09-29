20ba21f7ceafb77f639a7fcccc482aca3f04867e
Hanafy-ubuntu <ahmedhanfy1111@gmail.com>
2020-09-29 17:49:14 +0200

    log connect message,remove catch error catch error handled in the app.js with logging

--- startup/db.js
+++ startup/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+const winston = require('winston');
 const db_URI = 'mongodb://localhost:27017/MyUdemy';
 module.exports = function (){
     //======== db setup ================
     mongoose.connect(db_URI)
-    .then(()=>console.log('connected to mongodb'))
-    .catch((err)=>console.log(`can't connect` , err.message));
+    .then(()=>winston.info('connected to mongodb'));
+    /// we don't need to catch the error the error will be catched from process.on in app.js
 }
