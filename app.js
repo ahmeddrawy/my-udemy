2a792314e0f5645f298721d3a9f28e884322b13d
Hanafy-ubuntu <ahmedhanfy1111@gmail.com>
2020-09-29 18:21:19 +0200

    extract logging from app.js moving logging logic and express-async-error package to logging.js handle the unhandledExceptions using winston todo handle rejected promises using winston

--- app.js
+++ app.js
@@ -1,28 +1,9 @@
 const express = require('express');
 const app = express();
-const Joi = require('joi'); //return class
 const config = require('config');
-require('express-async-errors');
-const winston = require('winston');
-require('winston-mongodb');
+require('./startup/logging')(); /// logging is here to handle errors from routes and dbs
 require('./startup/routes')(app);
 require('./startup/db')();
-process.on('uncaughtException',(exc)=>{
-    winston.error(exc.message,exc);
-    console.log('we faced an internal error');
-    // process.exit(1);
-});
-const db_URI = 'mongodb://localhost:27017/MyUdemy';
-/// to log errors in files
-winston.add(new winston.transports.File({filename:'logfile.log'}));
-/// to log errors in mongodb 
-winston.add(new winston.transports.MongoDB({db:db_URI}));
-
-process.on('unhandledRejection' ,(exc)=>{
-    winston.error(exc.message,exc);
-    /// todo we should terminate here but process.exit doesn't wait for the logging process
-    /// we must wait for the streams to flush 
-});
 // config
 if(!config.get('jwt_private')){
     console.error('Fatal error: jwt_private is not defined !!');
---
+++ startup/logging.js
@@ -0,0 +1,27 @@
+const winston = require('winston');
+require('winston-mongodb');
+const db_URI = 'mongodb://localhost:27017/MyUdemy';
+require('express-async-errors');
+
+module.exports = function () {
+    winston.exceptions.handle(
+        new winston.transports.File({filename:'uncaughtExceptions.log'})
+    );
+    // process.on('uncaughtException',(exc)=>{ /// todo handle with winston
+    //     winston.error(exc.message,exc);
+    //     console.log('we faced an internal error');
+    //     // process.exit(1);
+    // });
+    
+    process.on('unhandledRejection' ,(exc)=>{
+        throw exc;
+        // winston.error(exc.message,exc);
+        // / todo we should terminate here but process.exit doesn't wait for the logging process
+        // / we must wait for the streams to flush 
+    });
+    /// to log errors in files
+    winston.add(new winston.transports.File({filename:'logfile.log'}));
+    /// to log errors in mongodb 
+    winston.add(new winston.transports.MongoDB({db:db_URI}));
+
+}
