a4d6f646d0a77cf2c7d2820ea8c47aed7e4d4a3c
Emma Slight <emma.slight@icloud.com>
2020-10-19 19:18:50 +1300

    set up view engine for handlebars

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require('./startup/config')()
 const app = require('./startup')
 const winston = require('winston')
+require('./startup/db')
+require('./startup/logging')() /// logging is here to handle errors from routes and dbs
 
 const port = process.env.PORT || 8080
 app.listen(port, () => {
