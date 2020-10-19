7d1718a456d49f7b8cc3ec99fa5f28515a3e9a6a
Emma Slight <emma.slight@icloud.com>
2020-10-19 19:15:59 +1300

    setup views for landing page

---
+++ controllers/course.controller.js
@@ -0,0 +1,33 @@
+const config = require('config')
+const { x } = require('joi')
+const Course = require('../models/course')
+
+module.exports = { showCourses: showCourses, seedCourses: seedCourses }
+
+//render all courses
+function showCourses(req, res) {
+  Course.find({})
+    .then((courses) => {
+      res.render('home', { courses: courses })
+    })
+    .catch((err) => {
+      res.status(500).send(err.message)
+    })
+}
+
+function seedCourses(req, res) {
+  const seedCourses = [
+    { name: 'Learn HTML', tags: ['code', 'beginner', 'HTML'] },
+    { name: 'Learn CSS', tags: ['code', 'beginner', 'CSS'] },
+    { name: 'Learn Javascript', tags: ['code', 'beginner', 'Javasript'] },
+    { name: 'Learn React', tags: ['code', 'beginner', 'React'] },
+    { name: 'Learn Vue', tags: ['code', 'beginner', 'Vue'] },
+  ]
+
+  for (course of seedCourses) {
+    var newCourse = new Course(course)
+    newCourse.save()
+  }
+
+  res.send('courses seeded')
+}
