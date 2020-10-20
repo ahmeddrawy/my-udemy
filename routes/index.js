0bd493e0b55722134e750e8e80bf0049bca4e388
Hanafy-ubuntu <ahmedhanfy1111@gmail.com>
2020-10-20 19:59:05 +0200

    fix a typo in login route path

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const courses = require('./courses')
 const logins = require('./logins')
 const genres = require('./genres')
 const users = require('./users')
-const login = require('./website/login')
+const login = require('./Website/login')
 const home = require('./home')
 
 module.exports = { courses, home, logins, genres, users, login }
