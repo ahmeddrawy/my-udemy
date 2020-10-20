a092aed6564ef33f30669c3f522d61f0e1c2a327
Hanafy-ubuntu <ahmedhanfy1111@gmail.com>
2020-10-20 19:58:18 +0200

    fix views path to render pages

--- startup/index.js
+++ startup/index.js
@@ -16,7 +16,7 @@ app.engine(
   })
 )
 app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, '../views'))
 app.use(express.static('public'))
 // app.use(express.json());
 app.use('/home', home)
