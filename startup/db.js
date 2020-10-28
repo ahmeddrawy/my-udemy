4a2bedebf6ff43f9efde88440baf34b26666a546
Hanafy-ubuntu <ahmedhanfy1111@gmail.com>
2020-10-28 14:13:14 +0200

    add db_url as env variable based on current env

--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 const winston = require('winston');
-const db_URI = 'mongodb://localhost:27017/MyUdemy';
+const db_URI =require('config').get('db') ;
+console.log(db_URI);
 module.exports = function (){
     //======== db setup ================
     mongoose.connect(db_URI,{ useUnifiedTopology: true , useNewUrlParser: true})
-    .then(()=>winston.info('connected to mongodb'));
+    .then(()=>winston.info(`connected to ${db_URI}`));
     /// we don't need to catch the error the error will be catched from process.on in app.js
 }
